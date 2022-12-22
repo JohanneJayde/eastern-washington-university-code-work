9e9b4a6d87d322d2bd49a71f9243c3e0c61787a1
Johanne McClenahan <114778753+JohanneJayde@users.noreply.github.com>
2022-12-22 12:50:06 -0800

    added CSCD210 Lab08

---
+++ CSCD210 - Programming Principles 1/Lab08-Arrays/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"lab8.cscd210utils","l":"ArrayUtils"},{"p":"lab8.cscd210lab8","l":"CSCD210Lab8"},{"p":"lab8.cscd210methods","l":"CSCD210Lab8Methods"},{"p":"lab8.cscd210utils","l":"IntStatisticsUtils"},{"p":"lab8.cscd210utils","l":"SortUtils"}]
