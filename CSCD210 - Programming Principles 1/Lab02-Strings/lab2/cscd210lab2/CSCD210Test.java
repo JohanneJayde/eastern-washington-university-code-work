64a0ee2ce53854cee553a1d92e897d8067c21739
Johanne McClenahan <114778753+JohanneJayde@users.noreply.github.com>
2022-12-22 12:42:41 -0800

    added CSCD210 Lab02

---
+++ CSCD210 - Programming Principles 1/Lab02-Strings/lab2/cscd210lab2/CSCD210Test.java
@@ -0,0 +1,15 @@
+package lab2.cscd210lab2;
+
+import java.util.Scanner;
+
+public class CSCD210Test
+{
+   
+   public static void main(String [] args)
+   {
+   
+   Scanner kb = new Scanner(System.in);
+   
+      
+   }
+}
