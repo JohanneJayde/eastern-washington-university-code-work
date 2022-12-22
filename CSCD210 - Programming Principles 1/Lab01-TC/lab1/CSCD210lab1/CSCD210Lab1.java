45b46dc35ff3c3ff7fef861cc52fda38f17f77a8
Johanne McClenahan <114778753+JohanneJayde@users.noreply.github.com>
2022-12-22 12:41:43 -0800

    added CSCD210 Lab01

---
+++ CSCD210 - Programming Principles 1/Lab01-TC/lab1/CSCD210lab1/CSCD210Lab1.java
@@ -0,0 +1,26 @@
+package lab1.CSCD210Lab1;
+
+import java.util.Scanner;
+
+public class CSCD210Lab1
+{
+	public static void main(String [] args)
+	{
+		int credits;
+		String name;
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.print("Please enter your name ");
+		name = kb.nextLine();
+		System.out.print("Please enter the number of credits you are taking ");
+		credits = kb.nextInt();
+		
+		System.out.println("Welcome " + name);
+		System.out.printf("Good luck taking %d%s\n", credits,
+		                   (credits != 1 && credits != -1) ? "credits" : "credit");		
+                         
+      kb.close();
+	
+	}// end main
+
+}// end class
